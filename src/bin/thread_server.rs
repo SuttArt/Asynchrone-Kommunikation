2f7615bd50e6a699d2bc28a2180781fd1c474bf8
Artem Suttar <artem.suttar@gmail.com>
2024-11-20 11:50:18 +0100

    Add threads to server
    - Server still can recieve just 1 message from each client
    - Server dont send messages back. Dont know why
    TODO:
    - write in vector each connected client, broadcast messages to each of
      them

--- src/bin/thread_server.rs
+++ src/bin/thread_server.rs
@@ -1,15 +1,27 @@
-use std::{io::{self, Read, Write}, env, net::{TcpListener, TcpStream}};
+use std::{io::{self, Read, Write}, env, net::{TcpListener, TcpStream}, thread};
+use std::sync::{Arc, Mutex};
 
 fn main() -> io::Result<()> {
 	let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
 	let listener = TcpListener::bind(addr)?;
 	
-	// TODO: implement threads for the server logic
 	println!("Bound to {:?}", listener);
-	for stream in listener.incoming() {
-		// println!("Connection attempt by {:?}", stream?);
 
-		let _ = handle_client(stream?);
+	let clients = Arc::new(Mutex::new(Vec::new()));
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				let clients = Arc::clone(&clients);
+				clients.lock().unwrap().push(stream.try_clone().unwrap());
+
+				thread::spawn(move || {
+					let _ = handle_client(stream);
+				});
+			}
+			Err(e) => {
+				eprintln!("Connection error: {:?}", e);
+			}
+		}
 	}
 	
 	Ok(())
