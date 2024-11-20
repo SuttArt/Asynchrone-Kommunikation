af76a0a726b2d4ba3052582d10307c9e2652c857
Artem Suttar <artem.suttar@gmail.com>
2024-11-20 10:40:43 +0100

    Add Client
    It may work, but its not tested yet, case its no server.
    But it can send 1 message to thread server and if we start more than 1
    client it is possible to send 1 message for each client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,15 +1,31 @@
-use std::{env, net::TcpStream, io::{self, Read, Write}};
+use std::{env, net::TcpStream, thread};
+use std::io::{self, Write, BufReader, BufRead};
 
 fn main() -> io::Result<()> {
 	let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
 	let mut _stream = TcpStream::connect(addr)?;
 
-	_stream.write(b"Hello, Server!")?;
+	// create clone stream, to be able to read and write at the same time
+	let stream_clone = _stream.try_clone()?;
 
-	let mut buffer = [0; 128];
-	let n = _stream.read(&mut buffer)?;
-	println!("Nachricht vom Server empfangen: {}", String::from_utf8_lossy(&buffer[..n]));
-	// TODO: implement client logic
-	
-	Ok(())
+	thread::spawn(move || {
+		let reader = BufReader::new(stream_clone);
+		for line in reader.lines() {
+			match line {
+				Ok(message) => println!("Server: {}", message),
+				Err(e) => {
+					eprintln!("Error when reading the message: {}", e);
+					break;
+				}
+			}
+		}
+	});
+
+	// Haupt-Thread liest Nachrichten von der Konsole und sendet sie an den Server
+	let mut input = String::new();
+	loop {
+		input.clear();
+		io::stdin().read_line(&mut input)?;
+		_stream.write_all(input.as_bytes())?;
+	}
 }
